2eef452a1fa02d797a9dfdfddd595a1078b902b0
awmcf <aaronmcfaull@gmail.com>
2019-02-20 15:19:46 +0000

    Test passes for findFilmByTitle

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,23 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.add = function (film) {
+  this.films.push(film);
+}
+
+Cinema.prototype.findFilmByTitle = function (title) {
+  const total = this.films.find( function (film) {
+    return film.title === title
+  });
+return total;
+}
+
 module.exports = Cinema;
+
+// for (const film of this.films) {
+//   if (film.title === title) {
+//     foundFilms.push(film);
+//   }
+// }
+// return foundFilms;
+// }
